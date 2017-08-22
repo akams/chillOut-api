276158e0e3d4b76807d435b6a78972a3b158c790
akams <e.ndjawe@gmail.com>
2017-08-23 01:21:27 +0200

    Feature(): renvoie un image aléatoire pour le mock

--- mock-api/mock-establishment.js
+++ mock-api/mock-establishment.js
@@ -1,14 +1,16 @@
-var faker = require('faker');
+var faker = require('faker/locale/fr');
 
 var listEstablishment = function() {
     return new Promise((resolve, reject) => {
         const companies = [];
+        const fakeImage = Object.keys(faker.image);
         for (var i = 0; i < 10; i++) {
+            const img = fakeImage[Math.floor(Math.random() * fakeImage.length)];
             companies.push({
                 id: i,
                 name: faker.company.companyName(),
                 description: faker.company.catchPhraseAdjective(),
-                img: faker.image.imageUrl(),
+                img: faker.image[img](640,480),
                 adress: {
                     city: faker.address.city(),
                     streetName: faker.address.streetName(),
